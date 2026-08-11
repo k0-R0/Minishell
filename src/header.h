9b49d40b6f48413ca62824b88054ee952f4b9d32
k0-R0 <bmenonprayush@gmail.com>
2026-08-11 10:57:06 +0530

    add extract_external_commands function

---
+++ src/header.h
@@ -0,0 +1,44 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define BUILTIN 1
+#define EXTERNAL 2
+#define NO_COMMAND 3
+
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+// default prompt is minishell
+void scan_input(char *prompt, char *input_string);
+// check for prompt update before using get_command
+// use strncmp
+char *get_command(char *input_string);
+
+// validate then update prompt
+// error = "command not found"
+void copy_change(char *prompt, char *input_string);
+
+int check_command_type(char *command);
+void echo(char *input_string, int status);
+void execute_internal_commands(char *input_string);
+void execute_external_commands(char *input_string);
+void signal_handler(int sig_num);
+void extract_external_commands(char **external_commands);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,71 +1,35 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "header.h"
 
-#include <fcntl.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdio_ext.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
+char *builtins[] = {
+    "echo",    "printf",   "read",    "cd",     "pwd",   "pushd",  "popd",
+    "dirs",    "let",      "eval",    "set",    "unset", "export", "declare",
+    "typeset", "readonly", "getopts", "source", "exit",  "exec",   "shopt",
+    "caller",  "true",     "type",    "hash",   "bind",  "help",   NULL};
 
-#define BUILTIN 1
-#define EXTERNAL 2
-#define NO_COMMAND 3
-
-#define ANSI_COLOR_RED "\x1b[31m"
-#define ANSI_COLOR_GREEN "\x1b[32m"
-#define ANSI_COLOR_YELLOW "\x1b[33m"
-#define ANSI_COLOR_BLUE "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN "\x1b[36m"
-#define ANSI_COLOR_RESET "\x1b[0m"
-
-void scan_input(char *prompt, char *input_string);
-char *get_command(char *input_string);
-
-void copy_change(char *prompt, char *input_string);
-
-int check_command_type(char *command);
-void echo(char *input_string, int status);
-void execute_internal_commands(char *input_string);
-void signal_handler(int sig_num);
-void extract_external_commands(char **external_commands);
-
-int read_external_cmd(const char *path, char *cmdv[]) {
-    int fd = open(path, O_RDONLY);
-    if (fd == -1) {
-        perror(NULL);
-        return 1;
+void extract_external_commands(char **cmdv) {
+    FILE *fptr = fopen("external_commands.txt", "r");
+    if (fptr == NULL) {
+        printf("Unable to open file\n");
+        return;
     }
     int cmd_ind = 0;
     char ch;
-    while (read(fd, &ch, 1) > 0) {
-        char *curr_cmd = malloc(100 * sizeof(char));
-        int ind = 0;
-        curr_cmd[ind++] = ch;
-        while (ch != '\n') {
-            if (read(fd, &ch, 1) > 0)
-                curr_cmd[ind++] = ch;
-        }
-        curr_cmd[ind] = '\0';
+    while (feof(fptr) == 0) {
+        char local_buffer[100];
+        if (fgets(local_buffer, 100, fptr) == NULL)
+            break;
+        printf("%s\n", local_buffer);
+        char *curr_cmd = malloc((strlen(local_buffer) + 1) * sizeof(char));
+        strcpy(curr_cmd, local_buffer);
         cmdv[cmd_ind++] = curr_cmd;
     }
     for (int i = 0; i < cmd_ind; i++) {
-        printf("%s\n", cmdv[i]);
+        printf("%d -> %s\n", i, cmdv[i]);
     }
-    return 0;
+    return;
 }
 
 int main() {
     char *cmdv[200];
-    if (read_external_cmd("external_commands.txt", cmdv)) {
-        printf("error reading file\n");
-        return 1;
-    }
+    extract_external_commands(cmdv);
 }
-
-#endif
